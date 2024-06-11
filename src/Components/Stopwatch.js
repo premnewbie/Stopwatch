f40c2873a5850ffdfec58149c036b895d93634bb
premnewbie <prempm50@gmail.com>
2024-06-11 18:54:05 +0530

    removed milliseconds

--- src/Components/Stopwatch.js
+++ src/Components/Stopwatch.js
@@ -34,13 +34,13 @@ const StopWatch = () => {
     const formatTime = () => {
         let minutes = Math.floor(timePassed/(1000*60)%60)
         let seconds = Math.floor(timePassed/(1000)%60)
-        let milliseconds = Math.floor(timePassed%(1000)/10)
+        // let milliseconds = Math.floor(timePassed%(1000)/10)
 
         minutes = String(minutes).padStart(2,'0');
         seconds = String(seconds).padStart(2,'0');
-        milliseconds = String(milliseconds).padStart(2,'0');
+        // milliseconds = String(milliseconds).padStart(2,'0');
 
-        return `${minutes}:${seconds}:${milliseconds}`;
+        return `${minutes}:${seconds}`;
     }
 
     return ( <>
