4427f7bee8c4b2e7b5219a31202a378f5d060543
premnewbie <prempm50@gmail.com>
2024-06-11 17:15:27 +0530

    stopwatch

---
+++ src/Components/Stopwatch.js
@@ -0,0 +1,50 @@
+import {useState,useEffect,useRef} from 'react';
+const StopWatch = () => {
+    const [isRunning,setIsRunning] = useState(false);
+    const [timePassed,setTimePassed] = useState(0);
+    const interval = useRef(null);
+    const startTime = useRef(0);
+
+    useEffect(()=>{
+        if(isRunning){
+            interval.current = setInterval(() => {
+                setTimePassed(Date.now()-startTime.current)
+            },10)
+        }
+
+        return () =>{
+            clearInterval(interval.current);
+        }
+    },[isRunning])
+
+    const start = () => {
+        setIsRunning(true);
+        startTime.current = Date.now() - timePassed;
+    }
+
+    const reset = () => {
+        setTimePassed(0);
+        setIsRunning(false);
+    }
+
+    const formatTime = () => {
+        let minutes = Math.floor(timePassed/(1000*60)%60)
+        let seconds = Math.floor(timePassed/(1000)%60)
+        let milliseconds = Math.floor(timePassed%(1000)/10)
+
+        minutes = String(minutes).padStart(2,'0');
+        seconds = String(seconds).padStart(2,'0');
+        milliseconds = String(milliseconds).padStart(2,'0');
+
+        return `${minutes}:${seconds}:${milliseconds}`;
+    }
+
+    return ( <>
+    <h1>Stopwatch</h1>
+    <div>Time: {formatTime()}</div>
+    <button onClick={start}>Start</button>
+    <button onClick={reset}>Reset</button>
+    </> );
+}
+ 
+export default StopWatch;
